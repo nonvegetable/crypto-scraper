3676fc377c5053596a400091d96004aa90cc5d58
Vighnesh Palande <100308933+nonvegetable@users.noreply.github.com>
2024-06-11 16:14:30 +0530

    Update models.py

--- scraper/models.py
+++ scraper/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Job(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    status = models.CharField(max_length=20, default='PENDING')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Task(models.Model):
+    coin = models.CharField(max_length=20)
+    data = models.JSONField(blank=True, null=True) 
+    job = models.ForeignKey(Job, on_delete=models.CASCADE, related_name='tasks')
